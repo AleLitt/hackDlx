ee6d612467944efe4e17a46dfb4cb38176070fd0
Alex Littorin <alex.littorin@basalt.se>
2024-02-14 11:06:29 +0100

    lolz

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 import requests
+import random
 
 app = Flask(__name__)
 
@@ -15,10 +16,24 @@ def get_trump_quote():
         print("Error:", response.status_code)
         return None
 
-@app.route('/trump-quote')
-def trump_quote():
-    quote = get_trump_quote()
-    return jsonify({"quote": quote})
+def get_kanye_quote():
+    response = requests.get("https://api.kanye.rest")
+    if response.status_code == 200:
+        data = response.json()
+        return data.get('quote')
+    else:
+        print("Failed to retrieve a quote")
+        return None
+
+@app.route('/random-quote')
+def random_quote():
+    if random.choice([True, False]):
+        quote = get_trump_quote()
+        source = "Trump"
+    else:
+        quote = get_kanye_quote()
+        source = "Kanye"
+    return jsonify({"quote": quote, "source": source})
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
